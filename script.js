22cf84bab254875bb4dac02d5191b30a7b71a32d
Max-Dor <la2web@hotmail.com>
2023-01-10 15:19:17 +0300

    Логичесские операторы. Усвоил

--- script.js
+++ script.js
@@ -1,25 +1,59 @@
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ?','');
-const a = prompt('Один из последних просмотренных фидьмов ...',''),
-        b = prompt('На сколько  оцените его по 10-ти бальной шкале ?',''),
-        c = prompt('Один из последних просмотренных фидьмов ...',''),
-        d = prompt('На сколько  оцените его по 10-ти бальной шкале ?','');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ?','');
+// const a = prompt('Один из последних просмотренных фидьмов ...',''),
+//         b = prompt('На сколько  оцените его по 10-ти бальной шкале ?',''),
+//         c = prompt('Один из последних просмотренных фидьмов ...',''),
+//         d = prompt('На сколько  оцените его по 10-ти бальной шкале ?','');
 
-const personalMoviesDB = {
-    count:numberOfFilms ,
-    movies: {
+// const personalMoviesDB = {
+//     count:numberOfFilms ,
+//     movies: {
 
-    },
-    actors: {
+//     },
+//     actors: {
 
-    },
-    genres: [
+//     },
+//     genres: [
 
-    ],
-    privat: false
-};
-personalMoviesDB.movies[a] = b;
-personalMoviesDB.movies[c] = d;
+//     ],
+//     privat: false
+// };
+// personalMoviesDB.movies[a] = b;
+// personalMoviesDB.movies[c] = d;
 
 
-console.log(personalMoviesDB)
+// console.log(personalMoviesDB)
+
+
+
+// if(4 == 5){
+//     console.log('ok')
+// }else{
+//     console.log('Равенство не верно')
+// }
+
+// const num  = 101;
+
+// if(num < 49){
+//     console.log('num  меньше 49 !')
+// }else if(num > 100){
+//     console.log('num  больше 100')
+// }else if(num == 50){
+//     console.log('Бинго  num = 50')
+// }
+
+
+// const hungry = true;
+// const frenchFrise = 3;
+// const hamburger = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+// if(hamburger === 3 && cola ==2  || frenchFrise == 3 && nuggets){
+//     console.log('true')
+// }else{
+//     console.log('false')
+// }
+// console.log(hamburger === 3 && cola ==2  || frenchFrise == 3 && nuggets)
+
+
