a3e62178f950fffbe171578ddec63043e44ac113
Max-Dor <la2web@hotmail.com>
2023-02-16 13:07:35 +0300

    Комитим  условия

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ?','');
-const a = prompt('Один из последних просмотренных фидьмов ...',''),
-        b = prompt('На сколько  оцените его по 10-ти бальной шкале ?',''),
-        c = prompt('Один из последних просмотренных фидьмов ...',''),
-        d = prompt('На сколько  оцените его по 10-ти бальной шкале ?','');
+let numberOfFilms;
+
+function start(){
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ?','');
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ?','');
+    }
+};
+start();
 
 const personalMoviesDB = {
     count:numberOfFilms ,
@@ -18,8 +22,26 @@ const personalMoviesDB = {
     ],
     privat: false
 };
-personalMoviesDB.movies[a] = b;
-personalMoviesDB.movies[c] = d;
 
+for(let i = 0; i < 2 ; i++){
+    const a = prompt('Один из последних просмотренных фидьмов ...',''),
+        b = prompt('На сколько  оцените его по 10-ти бальной шкале ?','');
+        
+    if(a != null && b != null && a != '' && b != '' && a.length < 50 ){
+        personalMoviesDB.movies[a] = b;
+        console.log('Done')
+    }else{ i--; }
+};
 
-console.log(personalMoviesDB)
+if(personalMoviesDB.count < 10 ){
+    console.log('Просмотрено слишком мало фильмов !')
+}else if(personalMoviesDB.count >= 10 && personalMoviesDB.count < 30 ){
+    console.log('Вы классический зритель !')
+}else if(personalMoviesDB.count >= 30){
+    console.log('Вы киноман !')
+}else{ 
+    console.log('Произошла ошибка !')
+};
+
+console.log(personalMoviesDB);
+// Проба пера . Проверка клонирования на ноут...
